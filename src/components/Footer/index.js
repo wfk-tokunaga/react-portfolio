ec75e45003f9f77ce2cc11f5758dc23568766ce1
wfk-tokunaga <wktokunaga@gmail.com>
2022-06-24 14:59:07 -0700

    updated README

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,11 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <p>Made by Will Tokunaga, 2022</p>
+            <div>
+                <span><a href="https://t.me/thrill_iam">Telegram</a></span>  
+                <span><a href="https://github.com/wfk-tokunaga">GitHub</a></span> 
+                <span><a href="https://www.linkedin.com/in/william-tokunaga-236819184/">LinkedIn</a></span> 
+            </div>
         </footer>
     );
 }
